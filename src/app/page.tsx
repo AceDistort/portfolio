a1f029a82ef43590ab1367040107246120940dd6
AceDistort <antonin.david79@gmail.com>
2024-03-14 16:55:54 +0100

    Home page presentation section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import LinkImage from "@/components/LinkImage";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/profile_picture.png"
-          alt="Photo de profil"
-          width={180}
-          height={180}
-          priority
-        />
+      <main>
+          <section className={styles.presentation}>
+              <div className="presentation-content flex-row flex-wrap align-center justify-between gap-5">
+                  <Image
+                      className={styles.logo}
+                      src="/profile_picture.png"
+                      alt="Photo de profil"
+                      width={250}
+                      height={250}
+                      priority
+                  />
+                  <div className="flex-column justify-start">
+                      <div>
+                          <h1 className={styles.title}>Antonin DAVID</h1>
+                          <h2 className={styles.subtitle}>Concepteur Développeur Fullstack</h2>
+                      </div>
+                      <div className="flex-row">
+                          <LinkImage href={process.env.LINK_LINKEDIN as string} src="/linkedin.svg" alt="LinkedIn"/>
+                          <LinkImage href={process.env.LINK_GITHUB as string} src="/github.svg" alt="GitHub"/>
+                      </div>
+                  </div>
+              </div>
+          </section>
     </main>
   );
 }
